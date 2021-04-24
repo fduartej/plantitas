2fcd2a11b9eb7b66e9d864ffe68e2e4fe69e1ebe
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-04-24 09:03:05 -0500

    Agregando el Flujo Contacto

--- Controllers/ContactoController.cs
+++ Controllers/ContactoController.cs
@@ -1,7 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace plantitas.Controllers
 {
     public class ContactoController
     {
-        
+        private readonly ILogger<ContactoController> _logger;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
