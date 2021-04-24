a5df1edf061e5edf1e14b668dd2f7cc42a410792
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-04-24 08:44:59 -0500

    Creacion Inicial Proyecto y reparticion Trabajo

---
+++ Models/Contacto.cs
@@ -0,0 +1,12 @@
+namespace plantitas.Models
+{
+    public class Contacto
+    {
+        public int ID {get; set;}
+
+        public String Name {get; set;}
+
+        public String Email {get; set;}
+        
+    }
+}
